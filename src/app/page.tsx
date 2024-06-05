7e84541376565f829a3a316d97ea7f276ac5e128
André Siboli <andresiboli@outlook.com>
2024-06-05 15:07:52 -0300

    Making some adjust on mobile - v0.0.2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,17 @@ import Duel from "@/app/components/game/Duel";
 import Game from "@/app/components/game/Game";
 import Score from "@/app/components/game/Score";
 import Rules from "@/app/components/game/Rules";
+import Container from "./components/layout/Container";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <Score />
-      <Game />
-      <Duel />
-      <Rules />
-    </main>
+    <Container>
+      <main className={styles.main}>
+        <Score />
+        <Game />
+        <Duel />
+        <Rules />
+      </main>
+    </Container>
   );
 }
